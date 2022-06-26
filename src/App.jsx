07ee03f004418d325c4ca6e72666374dc60b1a8e
TachibanaKimika <hlx12155326@gmail.com>
2022-06-27 00:33:27 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import Config from '@/components/Config'
+import { CHART_TYPES } from '@/config/index'
+import LineChart from '@/components/LineChart'
+import ColorBlock from '@/components/ColorBlock'
+export default function Home() {
+  const [colors, setColors] = useState([])
+  const [chartConfig, setChartConfig] = useState(null)
+  const [chartType, setChartType] = useState(CHART_TYPES.HUE)
+  return (
+    <>
+      <div style={{
+        display: 'flex',
+        minWidth: '1300px',
+      }}>
+        <div className="side-bar" style={{
+          width: '350px',
+          borderRight: '1px solid #777'
+        }}>
+          <Config 
+            colorChange={(data) => setColors(data)}
+            typeChange={(type) => setChartType(type)} />
+        </div>
+        <div className="main-view" style={{
+          width: 'calc(100% - 350px)',
+          padding: '30px',
+          boxSizing: 'border-box'
+        }}>
+          <div className="chart-view" style={{
+            width: '100%',
+          }}>
+            <ColorBlock colors={colors} />
+            <LineChart 
+            type={chartType}
+            data={colors} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
